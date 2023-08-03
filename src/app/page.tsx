e98bc6c7e3ea79fbe2ecc7045882095cc10c443c
Matheus Henrique <matheus.181137@gmail.com>
2023-08-03 12:25:13 -0300

    feat: add css reset

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
+import styles from "./page.module.css";
+
 export default function Home() {
-  return (
-    <main>
-      web
-    </main>
-  )
+  return <main className={styles.main}>web</main>;
 }
